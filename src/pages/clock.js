ce1f1430eb36fd0f65971c0363e4cab034ee09ad
carllosaguiar <carlosaguiar23@gmail.com>
2020-10-18 11:45:30 -0300

    :sparkles:Implemented Timer function and improvment layout:lipstick:

--- src/pages/clock.js
+++ src/pages/clock.js
@@ -6,10 +6,10 @@ import '../../src/App.css'
 
 const Clock = () => (
     <header className="App-header">
-        <h4>Clock</h4>
+        <h2>Clock</h2>
         <TimeZoneTable timeZoneData={timeZoneData} />
         <p className="App-text-center">
-          <smal>Time Zone</smal>  
+          <smal>Local Time Zone and more cities</smal>  
         </p>
         <Footer />
     </header>
--- src/pages/stopwatch.js
+++ src/pages/stopwatch.js
@@ -5,7 +5,7 @@ import Footer from '../../src/pages/partials/Footer'
 
 const ReturnStopwatch = () => (
     <header className="App-header">
-        <h4>Stopwatch</h4>
+        <h2>Stopwatch</h2>
         <Stopwatch />
         <p>This is stopwatch with some functions</p>
         <Footer />
